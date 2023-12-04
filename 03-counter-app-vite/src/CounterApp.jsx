4c2702ac9b5c3fa0d6ba2d5e192c1082a507e36a
facundo.rodriguez.g <facundo.rodriguez.g@estudiantes.utec.edu.uy>
2023-12-04 13:43:10 -0300

    handelReset added

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -12,12 +12,17 @@ export const CounterApp = ({ value }) => {
   const handleSubs = () => {
     setCounter((c) => c - 1); 
   };
+
+  const handleReset = () => {
+    setCounter((c) => c = value); 
+  };
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{counter}</h2> {/*Para manejar los cambios se utiliza el 'counter'*/}
       <button onClick={handleAdd}>+1</button>
       <button onClick={handleSubs}>-1</button>
+      <button onClick={handleReset}>RESET</button>
     </>
   );
 };
